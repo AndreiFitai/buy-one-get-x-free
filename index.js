83f982432a61fadb49032e8bf1a4d8b38436ce95
Andrei Fitai <fitai.andrei@gmail.com>
2021-04-12 12:21:36 +0200

    add input folder and read orders from it

--- index.js
+++ index.js
@@ -1,5 +1,26 @@
-function processOrders() {
-  console.log("Hello world !");
+const path = require("path");
+const fs = require("fs");
+const csv = require("csvtojson");
+
+async function processOrders() {
+  const filesNames = await fs.readdirSync(
+    path.resolve("input"),
+    (err, files) => {
+      if (err) {
+        throw new Error(`GetInputFilePaths: Unable to scan directory: ${err}`);
+      }
+
+      return files;
+    }
+  );
+
+  const filePaths = filesNames.map((file) => path.resolve("input", file));
+
+  const parsedCSVs = filePaths.map((filePath) => csv().fromFile(filePath));
+
+  const allOrders = await Promise.all(parsedCSVs);
+
+  console.log(allOrders.flat());
 }
 
 processOrders();
